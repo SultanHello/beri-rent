20da1538dffec1afcd8ddc9edf559bbe6ac983c5
SultanHello <108612675+SultanHello@users.noreply.github.com>
2026-02-04 20:59:16 +0500

    feign interface for payment service

--- payment-service/src/main/java/sultan/org/paymentservice/client/BookingClient.java
+++ payment-service/src/main/java/sultan/org/paymentservice/client/BookingClient.java
@@ -1,6 +1,7 @@
 package sultan.org.paymentservice.client;
 
 import jdk.jshell.Snippet;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
