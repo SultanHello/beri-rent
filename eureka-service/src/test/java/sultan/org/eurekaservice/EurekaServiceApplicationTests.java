3cdbf8bc8189faf1a4d9958bae41fd94165080b2
SultanHello <108612675+SultanHello@users.noreply.github.com>
2026-03-02 21:16:25 +0500

    try adding eureka

---
+++ eureka-service/src/test/java/sultan/org/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sultan.org.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
