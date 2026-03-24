f5a430b8dd11857cec9746452486fd2d560b824d
SultanHello <108612675+SultanHello@users.noreply.github.com>
2026-03-24 23:36:30 +0500

    check and refactor eureka

--- eureka-service/src/main/java/sultan/org/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/sultan/org/eurekaservice/EurekaServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer  // 👈 Включаем Eureka Server
+@EnableEurekaServer
 public class EurekaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
